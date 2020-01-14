5fffd5ef3124063a37c85f26d96e6ff0dd90bbff
池边树下 <mzky@163.com>
2020-01-14 18:17:55 +0800

    Create main.go

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"utils/log"
+)
+
+func main() {
+	log.Info("info")
+	log.Debug("debug")
+	log.Warn("warn")
+	log.Error("error")
+	log.Fatal("fatal")
+	log.Info("info")
+}
