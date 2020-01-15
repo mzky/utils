b1df044f4489a26b4c4615cd42c79fcb66760e90
池边树下 <mzky@163.com>
2020-01-15 10:43:06 +0800

    Update main.go

--- main.go
+++ main.go
@@ -5,10 +5,20 @@ import (
 )
 
 func main() {
+
+	logger, err := log.New("sentry", "debug", "")
+	if err != nil {
+		log.Fatal("%s", err)
+	}
+	log.Export(logger)
+
 	log.Info("info")
 	log.Debug("debug")
 	log.Warn("warn")
 	log.Error("error")
+	log.ReceiveMsg("ReceiveMsg")
+	log.FightValueChange("FightValueChange")
+	log.SendMsg("SendMsg")
+	log.Recover("Recover")
 	log.Fatal("fatal")
-	log.Info("info")
 }
