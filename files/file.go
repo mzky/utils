dbdd5a38c98c2123c871b76d6d6c0cb7784c3866
zhengkeyan@bjca.org.cn <zhengkeyan@bjca.org.cn>
2020-07-08 16:38:32 +0800

    优化

--- files/file.go
+++ files/file.go
@@ -1,30 +1,34 @@
 package util
 
 import (
-	"fmt"
 	"io/ioutil"
 	"os"
 	"path"
 	"path/filepath"
 	"strings"
-	"time"
 )
 
+//获取当前目录
+func SelfPath() string {
+	path, _ := filepath.Abs(os.Args[0])
+	return path
+}
+
 //获取指定目录下的所有文件和目录,不包含子目录
-func GetFilesAndDirs(dirPth, filter string) (files []string, dirs []string, err error) {
-	dir, err := ioutil.ReadDir(dirPth)
+func FilesAndDirs(fp, filter string) (files []string, dirs []string, err error) {
+	dir, err := ioutil.ReadDir(fp)
 	if err != nil {
 		return nil, nil, err
 	}
 
 	PthSep := string(os.PathSeparator)
-	//suffix = strings.ToUpper(suffix) //忽略后缀匹配的大小写
+	filter = strings.ToUpper(filter) //忽略后缀匹配的大小写
 
 	for _, fi := range dir {
-		dp := path.Join(dirPth, PthSep, fi.Name())
+		dp := path.Join(fp, PthSep, fi.Name())
 		if fi.IsDir() { // 目录, 递归遍历
 			dirs = append(dirs, dp)
-			GetFilesAndDirs(dp, filter)
+			FilesAndDirs(dp, filter)
 		} else {
 			// 过滤指定格式
 			ok := strings.HasSuffix(fi.Name(), filter)
@@ -38,21 +42,21 @@ func GetFilesAndDirs(dirPth, filter string) (files []string, dirs []string, err 
 }
 
 //获取指定目录下的所有文件,包含子目录下的文件
-func GetAllFiles(dirPth, filter string) (files []string, err error) {
+func FindAllFiles(fp, filter string) (files []string, err error) {
 	var dirs []string
-	dir, err := ioutil.ReadDir(dirPth)
+	dir, err := ioutil.ReadDir(fp)
 	if err != nil {
 		return nil, err
 	}
 
 	PthSep := string(os.PathSeparator)
-	//suffix = strings.ToUpper(suffix) //忽略后缀匹配的大小写
+	filter = strings.ToUpper(filter) //忽略后缀匹配的大小写
 
 	for _, fi := range dir {
-		dp := path.Join(dirPth, PthSep, fi.Name())
+		dp := path.Join(fp, PthSep, fi.Name())
 		if fi.IsDir() { // 目录, 递归遍历
 			dirs = append(dirs, dp)
-			GetAllFiles(dp, filter)
+			FindAllFiles(dp, filter)
 		} else {
 			// 过滤指定格式
 			ok := strings.HasSuffix(fi.Name(), filter)
@@ -64,7 +68,7 @@ func GetAllFiles(dirPth, filter string) (files []string, err error) {
 
 	// 读取子目录下文件
 	for _, table := range dirs {
-		temp, _ := GetAllFiles(table, filter)
+		temp, _ := FindAllFiles(table, filter)
 		for _, temp1 := range temp {
 			files = append(files, temp1)
 		}
@@ -73,42 +77,32 @@ func GetAllFiles(dirPth, filter string) (files []string, err error) {
 	return files, nil
 }
 
-//废弃,兼容旧版
-func FindFile(filePath string) bool {
-	_, err := os.Stat(filePath)
-	if os.IsNotExist(err) {
-		return false
-	}
-	return true
-}
-
 //判断文件夹或文件是否存在
-func IsExist(path string) bool {
-	_, err := os.Stat(path)
-	return os.IsExist(err)
+func IsExist(fp string) bool {
+	_, err := os.Stat(fp)
+	return err == nil || os.IsExist(err)
 }
 
 //获取文件修改时间 返回unix时间戳
-func GetFileModTime(path string) time.Time {
-	f, err := os.Open(path)
+func FileModTime(fp string) (int64, error) {
+	f, err := os.Stat(fp)
 	if err != nil {
-		fmt.Println("打开文件失败：" + path)
-		return time.Now()
+		return 0, err
 	}
-	defer f.Close()
-
-	fi, err := f.Stat()
-	if err != nil {
-		fmt.Println("无法获取文件信息：" + path)
-		return time.Now()
-	}
-
-	return fi.ModTime()
+	return f.ModTime().Unix(), nil
 }
 
-//绝对路径、目录、文件名、后缀
-func GetFilePathInfo(path string) (string, string, string) {
+//return 目录,文件名,后缀
+func FilePathInfo(fp string) (string, string, string) {
 	//filepath.Base(path)
-	dir, name := filepath.Split(path)    //目录、文件名
-	return dir, name, filepath.Ext(path) //后缀
+	dir, name := filepath.Split(fp)    //目录、文件名
+	return dir, name, filepath.Ext(fp) //后缀
+}
+
+func FileSize(fp string) (int64, error) {
+	f, e := os.Stat(fp)
+	if e != nil {
+		return 0, e
+	}
+	return f.Size(), nil
 }
