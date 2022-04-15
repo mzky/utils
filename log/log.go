ec1775dabe9efcd300ba0d203cccca585205815d
池边树下 <mzky@163.com>
2022-04-15 15:36:32 +0800

    Update log.go

--- log/log.go
+++ log/log.go
@@ -7,7 +7,7 @@ import (
 
 // InitLog 先初始化配置，使用的时候直接使用logrus.x()
 func InitLog(level string, logPath string) {
-	layout := `%t{yyyy-MM-dd HH:mm:ss.SSS} [%-5l{length=5}] ☆ %msg ☆ %caller %fields%n`
+	layout := `%t{yyyy-MM-dd HH:mm:ss.SSS} [%-5l{length=5}] ☆ %msg ☆ %caller{skip=1} %fields%n`
 	spec := fmt.Sprintf("level=%s,file=%s,maxSize=10M,maxAge=1095d,gzipAge=3d,stdout=true", level, logPath)
 	golog.Setup(golog.Layout(layout), golog.Spec(spec))
 }
